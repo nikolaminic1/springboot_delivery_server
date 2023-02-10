e9c370483e4335ad7f9b75f21c875489b467e67a
minicnikola1 <minicnikola999@gmail.com>
2023-02-11 00:01:00 +0100

    commit

--- src/main/java/com/example/delivey/DeliveryApplication.java
+++ src/main/java/com/example/delivey/DeliveryApplication.java
@@ -1,15 +1,19 @@
 package com.example.delivey;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class DeliveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryApplication.class, args);
 	}
 
 }
+
